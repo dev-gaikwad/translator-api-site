5b3e202c2b291f4e95cd9ab3685fde368ab5c894
dev-gaikwad <dev-gaikwad@users.noreply.github.com>
2022-12-16 20:00:33 +0530

    feat; error handling

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var btnTranslateMinion = document.querySelector("#btn-translate-minion");
-var btnTranslateYoda = document.querySelector("#btn-translate-yoda");
-var btnTranslateKlingon = document.querySelector("#btn-translate-klingon");
+var btnTranslateMinion = document.querySelector('#btn-translate-minion');
+var btnTranslateYoda = document.querySelector('#btn-translate-yoda');
+var btnTranslateKlingon = document.querySelector('#btn-translate-klingon');
 
-var txtInput = document.querySelector("#txt-input");
-var outputBlock = document.querySelector("#output-block");
-
-
+var txtInput = document.querySelector('#txt-input');
+var outputBlock = document.querySelector('#output-block');
 
 // var url = "https://lessonfourapi.tanaypratap.repl.co/translate/"
- var url = "https://api.funtranslations.com/translate/"
+var url = 'https://api.funtranslations.com/translate/';
 
-function constructURL(text,charachterSelect){
-    var updatedURL = url + charachterSelect + ".json?text=" + text ;
-    return updatedURL;
-
+function constructURL(text, charachterSelect) {
+  var updatedURL = url + charachterSelect + '.json?text=' + text;
+  return updatedURL;
 }
 
+function clickHandler(charachterSelect) {
+  var inputText = txtInput.value;
+  console.log(charachterSelect);
+  console.log(inputText);
 
-function clickHandler(charachterSelect){
-
-
-    var inputText = txtInput.value ;
-    console.log(charachterSelect);
-    console.log(inputText);
-
-    fetch(constructURL(inputText,charachterSelect))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated ;
-        outputBlock.innerText = translatedText ;
-    
+  fetch(constructURL(inputText, charachterSelect))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputBlock.innerText = translatedText;
     })
-    
-
-
+    .catch((error) =>
+      alert(
+        `Error \nProbably the api call limit reached \nPlease try after sometime`
+      )
+    );
 }
 
-btnTranslateMinion.addEventListener("click", function(){clickHandler("minion")})
-btnTranslateYoda.addEventListener("click", function(){clickHandler("yoda")})
-btnTranslateKlingon.addEventListener("click", function(){clickHandler("klingon")})
+btnTranslateMinion.addEventListener('click', function () {
+  clickHandler('minion');
+});
+btnTranslateYoda.addEventListener('click', function () {
+  clickHandler('yoda');
+});
+btnTranslateKlingon.addEventListener('click', function () {
+  clickHandler('klingon');
+});
